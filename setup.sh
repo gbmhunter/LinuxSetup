6e671226e5ce292617efe42950090f64e50ca81d
Geoffrey Hunter <gbmhunter@gmail.com>
2017-06-25 23:12:12 -0700

    Initial commit. Added basic setup script and utilities file.

---
+++ setup.sh
@@ -0,0 +1,89 @@
+#!/usr/bin/env bash
+
+# Any subsequent commands which fail will cause the shell script to exit immediately
+set -e
+
+# Get script path
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# User imports
+source "$script_dir/utilities.sh"
+
+
+printInfo "=========================================================================================="
+printInfo "=================================== Linux Setup Script ==================================="
+printInfo "=========================================================================================="
+
+
+# =============================================================================================== #
+# ================================ WHAT UBUNTU VERSION ARE WE RUNNING? ========================== #
+# =============================================================================================== #
+
+UBUNTU_VERSION=`lsb_release -sd`
+# This should return something along the lines of "Ubuntu 17.04"  
+
+IFS=' ' read -r -a array <<< ${UBUNTU_VERSION}
+DISTRO_NAME=${array[0]}
+DISTRO_VERSION=${array[1]}
+
+echo "DISTRO_NAME = ${DISTRO_NAME}"
+echo "DISTRO_VERSION = ${DISTRO_VERSION}"
+
+# Make sure the user is running this setup script on Ubuntu!
+if ! [[ ${DISTRO_NAME} == "Ubuntu" ]]; then
+	printError "ERROR: Distribution name was not Ubuntu."
+	exit 1
+fi
+
+# =============================================================================================== #
+# ========================================= INSTALL GIT ========================================= #
+# =============================================================================================== #
+
+if ! type "git" > /dev/null; then
+	printInfo "Installing git..."
+	sudo apt-get install -y git
+else
+	printInfo "git detected on system, not installing."
+fi
+
+# =============================================================================================== #
+# ==================================== INSTALL SUBLIME TEXT 3 =================================== #
+# =============================================================================================== #
+
+# Not crucial, but a useful tool to have!
+if ! type "subl" > /dev/null; then
+	printInfo "Installing sublime text 3..."
+	sudo add-apt-repository -y ppa:webupd8team/sublime-text-3
+	sudo apt-get update
+	sudo apt-get install -y sublime-text-installer
+else
+	printInfo "subl detected on system, not installing."
+fi
+
+# =============================================================================================== #
+# ======================================== INSTALL CMAKE ======================================== #
+# =============================================================================================== #
+
+# CMake for performing C/C++ builds
+if ! type "cmake" > /dev/null; then
+	printInfo "Installing cmake..."
+	sudo apt-get install -y cmake
+else
+	printInfo "cmake detected on system, not installing."
+fi
+
+# =============================================================================================== #
+# ======================================== .bashrc CONFIG ======================================= #
+# =============================================================================================== #
+
+# Add useful history seach to up/down arrow keys
+echo "bind \"\\\"\e[A\\\": history-search-backward\"" >> ~/.bashrc
+echo "bind \"\\\"\e[B\\\": history-search-forward\"" >> ~/.bashrc
+
+# =============================================================================================== #
+# ======================================= WORKSPACE CONFIG ====================================== #
+# =============================================================================================== #
+
+# Increase default workspace (1) to a 2x3 grid
+gsettings set org.compiz.core:/org/compiz/profiles/unity/plugins/core/ hsize 2
+gsettings set org.compiz.core:/org/compiz/profiles/unity/plugins/core/ vsize 3
